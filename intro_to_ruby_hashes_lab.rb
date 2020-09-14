74427bd4edcdcd1e8ae1f5122601e582043c77e4
Brock Byrd <brockbyrdd@gmail.com>
2020-09-14 18:35:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,6 +53,13 @@ def monopoly_with_fourth_tier
     railroads: 
     {
       pieces: 4, 
+      rent_in_dollars: 
+      {
+      one_piece_owned: 25,
+      two_pieces_owned: 50,
+      three_pieces_owned: 100,
+      four_pieces_owned: 200
+      }
       names: 
       {
         reading_railroad: {
@@ -68,13 +75,6 @@ def monopoly_with_fourth_tier
           "mortgage_value"=> "$800"
         }
       }, 
-      rent_in_dollars: 
-      {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-      }
     }
   }
 end
